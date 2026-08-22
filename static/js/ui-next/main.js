70a1f326b984183db21ca282cae5f44aca2bd63c
Josh Shapiro <kep359@gmail.com>
2026-08-21 23:24:11 -0600

    feat(ui): establish semantic visual system

--- static/js/ui-next/main.js
+++ static/js/ui-next/main.js
@@ -1,7 +1,10 @@
+import { initAccessibility } from "../ui/accessibility.js";
+
 const root = document.documentElement;
 
 if (root.dataset.uiNextEntry !== "development") {
   throw new Error("Replacement interface entry marker is missing.");
 }
 
+initAccessibility();
 root.dataset.uiNextReady = "true";
